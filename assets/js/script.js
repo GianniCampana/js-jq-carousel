47381fda02364b4d35cd627de32368c9d5e8425a
GianniCampana <campanagianni97@gmail.com>
2021-04-21 15:21:03 +0200

    immagini scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ $(function(){
     $('.next').click(nextImg);
     $('.prev').click(prevImg);
 
-
+    /* funzione immagine successiva */
   function nextImg(){
       /* referenza immagine attiva */
       var imgActive = $('.central img.active');
@@ -21,11 +21,11 @@ $(function(){
          imgActive.next('img').addClass('active');
          circleActive.next('i').addClass('active');
       }
-      
-      
-      
   }
 
+
+
+    /* funzione immagine precedente */
   function prevImg(){
     var imgActive = $('.central img.active');
     var circleActive = $('.cont_balls i.active');
@@ -40,5 +40,41 @@ $(function(){
       }
   }
 
+
+
+  /* per generare i pallini devo sapere quante foto ci sono */
+   /* col ciclo each() percorro tutta la collection di immagini e ottengo l'index che mi serve per capire quale è il primo (===0) */
+
+   $('.central img').each(function(index, element){
+     var cont_balls = $('.cont_balls');
+     /* creo una stringa vuota perche di default non c'è la classe active */
+     var active = "";
+     /* diventa active solo nell elemento 0 */
+     if(index === 0) active = "active";
+     /* genero la stringa da stampare */
+     var tag = '<i class="fas fa-circle ' + active + '"></i>';
+     /* la appendo a cont_balls */
+     cont_balls.append(tag);
+
+   });
+
+   
+/* aggiungo un listner click a tutti i (.cont_balls i) generati dinamicamente */
+$('.cont_balls i').on('click', function() {
+
+  /* l'index dell elemento cliccato mi serve per cercare la img corrispondente */
+  var index = $(this).index();
+
+  /* rimuovo active dagli elementi attivi */
+  $('.cont_balls i.active').removeClass('active');
+  $('.central img.active').removeClass('active');
+
+  /* con eq cerco l'elemento con indice index e gli aggiungo la classe active */
+  $('.central img').eq(index).addClass('active');
+  /* all elemento cliccato aggiungo la classe active */
+  $(this).addClass('active');
+
+});
+  
 
 });
